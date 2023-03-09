a11b3cd9942f575b9fdc820fa7d1f4bf264da494
Evertt Almeida <evertialmeida@gmail.com>
2023-03-09 01:45:33 -0300

    Sistema Gerenciador de Login inicial

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿namespace SistemaGerenciadorDeLogin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ConfirmarOuNegarConta, LembrarSenha; string Email, Senha, SenhaNovamente;
+
+            Console.WriteLine("Vc já tem uma conta? (S/N)");
+            ConfirmarOuNegarConta = Console.ReadLine().ToUpper();
+
+            if (ConfirmarOuNegarConta == "S")
+            {
+                Console.WriteLine("Se lembra da senha?(S/N)");
+                LembrarSenha = Console.ReadLine().ToUpper();
+
+                if (LembrarSenha == "S")
+                {
+                    Console.Write("E-mail: ");
+                    Email = Console.ReadLine();
+                    if (Email.Substring(Email.Length- 10)=="@gmail.com")
+                    {
+                        Console.Write("Senha: ");
+                        Senha = Console.ReadLine();
+                        Console.WriteLine("O E-mail: " + Email + "/ Senha: " + Senha);
+                    }
+                    else
+                    {
+                        Console.WriteLine("E-mail invalido!");
+                    }
+                }
+                else if (LembrarSenha == "N")
+                {
+                    Console.Write("Coloque seu E-mail para recriar outra conta: ");
+                    Email = Console.ReadLine();
+                    Console.WriteLine("Pronto.");
+                }
+
+            }
+            else if (ConfirmarOuNegarConta == "N")
+            {
+                Console.Write("Digite o seu melhor E-mail: ");
+                Email=Console.ReadLine();
+                do
+                {
+                    Console.Write("Digite uma senha: ");
+                    Senha = Console.ReadLine();
+                    Console.Write("Digite novamente: ");
+                    SenhaNovamente = Console.ReadLine();
+                    if (Senha != SenhaNovamente)
+                    {
+                        Console.WriteLine("Elas não estão iguais.");
+                    }
+                    else if (Senha.Length < 5)
+                    {
+                        Console.WriteLine("Não pode senha com menos que 5 caracateres.");
+                    }
+                } while (Senha != SenhaNovamente || Senha.Length < 5);
+                Console.WriteLine("Tudo certo! Conta e Login feito");
+                Console.WriteLine("O E-mail: " + Email + "/ Senha: " + Senha);
+            }
+
+
+        }
+    }
+}
